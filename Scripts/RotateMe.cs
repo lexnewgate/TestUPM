0b3adf7cca927884e6c466dd5b44f90cfff20629
weiwuhao <weiwuhao@blackjack-inc.com>
2023-11-18 02:12:40 +0800

    提交demo工程

---
+++ Scripts/RotateMe.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class RotateMe : MonoBehaviour
+{
+    public float angularVelocity = 10;
+    void Update ()
+    {
+        var rot = Quaternion.AngleAxis (angularVelocity * Time.deltaTime, Vector3.up);
+        transform.localRotation = rot * transform.localRotation;
+    }
+} 
